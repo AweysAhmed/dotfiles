a9987263a14be511f9239fed83ca5485507a95a4
Aweys Ahmed <aweys.n.ahmed@gmail.com>
2024-05-04 11:52:23 -0400

    added auto-completions

--- .config/nvim/lua/core/plugin_config/lsp_config.lua
+++ .config/nvim/lua/core/plugin_config/lsp_config.lua
@@ -1,11 +1,12 @@
 require("mason").setup()
 require("mason-lspconfig").setup({
-  ensure_installed = { "standardrb" }
+ ensure_installed = { "solargraph" }
 })
 
 local capabilities =  require("cmp_nvim_lsp").default_capabilities()
 
-require("lspconfig").standardrb.setup {
+require("lspconfig").solargraph.setup {
   capabilities = capabilities 
-
 }
+
+
--- .config/nvim/lua/core/plugin_config/treesitter.lua
+++ .config/nvim/lua/core/plugin_config/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
 
-  ensure_installed = { "ruby", "vim", "lua", "javascript" },
+  ensure_installed = { "ruby", "vim", "lua", "javascript", "html" },
 
   sync_install = false,
   auto_install = true,
